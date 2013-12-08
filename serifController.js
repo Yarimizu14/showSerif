2ad47adcb7e227891f6e817ad9513a5df38ed104
Kensei Yarimizu <kenseiyarimizu@Kensei-no-MacBook-Pro.local>
2013-12-08 23:46:38 +0900

    ほぼ完成版

---
+++ serifController.js
@@ -0,0 +1,108 @@
+/*
+ * 
+ */
+function serifController() {
+    this.initialize.apply(this, arguments);
+}
+
+serifController.prototype.initialize = function() {
+    this.timerId   = null;
+    this.nodeStack = [];
+    this.callback  = null;
+}
+
+serifController.prototype.showSerif = function(origin, target, callback) {
+    this.initialize();
+    this.parseDom(origin, $(target));
+    this.callback = callback;
+    this.next();
+}
+serifController.prototype.showText = function($text, text,  callback) {
+  var textIndex = 1;
+  var self = this;
+
+  var showOneTxt = function() {
+    $text[0].data = text.substring(0, textIndex);
+
+    if (textIndex >= text.length) {
+      if (typeof callback == "function")  callback();
+      return;
+    }
+
+    textIndex++;
+    self.timerId = setTimeout(function() {
+      showOneTxt();
+    }, 100);
+  }
+  showOneTxt();
+}
+
+serifController.prototype.next = function() {
+  var next = this.nodeStack.shift();
+  if (next) {
+    this.show(next);
+    return true;
+  } else {
+    this.callback();
+    return false;
+  }
+}
+
+serifController.prototype.show = (function() {
+    var $text, parent;
+
+    return function($elem) {
+      parent = $elem.data("parent");
+      $text = $($elem.data("text"));      //テキストノードを取得
+
+      if (!$text.length) {  //タグの場合:アペンドし次のノードへ進む
+        parent.append($elem);
+        this.next();
+      } else {      //テキストノードの場合
+        text = $text.text().replace(/\s/g, "");
+        parent.append($text);
+        var self = this;
+        this.showText($text, text, function() {
+          self.next();
+        });
+      };
+    };
+})();
+
+/*
+   parseDom
+   @params {HTMLCollection} elem
+   @params {jQuery Obj}     parent
+*/
+serifController.prototype.parseDom = function(elem, parent) {
+    var $elem = $(elem);
+    var contents = ($elem && $elem.contents) ? $elem.contents() : $elem; //returns jQuery Object
+    var self = this;
+
+    if (contents && contents.length >= 1) {
+        var $tag = $($("<div>").append($elem.clone().html("")).html());  //タグのみを取得
+        $tag.data("parent", parent); //親タグの情報を追加
+        self.nodeStack.push($tag);
+        return $.map(contents.get(), function(val) {
+            return self.parseDom(val, $tag);
+        });
+    } else {
+        var text = $elem.text().replace(/\s/g, "") ;
+        if (text) {
+            var textContainer = $("<div>"); //textNodeを運ぶ仮のDivタグを作成(*textNodeのjQueryオブジェクトではdataメソッドを使用出来ない)
+            //textContainer.data("text", text);
+            textContainer.data("text", elem);
+            textContainer.data("parent", parent);
+            self.nodeStack.push(textContainer);
+        }
+        return elem;
+    }
+}
+
+serifController.prototype.stopShowing = function() {
+    if (this.timerId)
+        clearTimeout(this.timerId);
+    delete this.nodeStack;
+}
+
+
